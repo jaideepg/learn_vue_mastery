d035bd4dbdaaca25bd35a41df449e27ad22ffc35
Jaideep Gajerawala <jaideepgajerawala@Jaideeps-MacBook-Pro.local>
2024-06-22 16:27:09 -0500

    added v for  and key concepts

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+            image: './assets/images/socks_blue.jpg',
             inStock: true,
-            inventory: 10,
-            onSale: true
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+              ]
         }
     }
 })
